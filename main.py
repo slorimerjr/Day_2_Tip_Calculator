fd03d3a218f312a3fa2d8c9f8904647888e86b54
Sam Lorimer <slorimerjr@gmail.com>
2021-11-14 19:48:01 +0000

    Completed the tip calculator.

--- main.py
+++ main.py
@@ -6,3 +6,29 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
+# Welcome to the tip calculator!
+# What was the total bill? $124.56
+# How much tip would you like to give? 10, 12, or 15? 12
+# How many people to split the bill? 7
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15: "))
+people = int(input("How many people to split the bill? "))
+amount = ((bill * (tip / 100) + bill) / people)
+amount_per_person = "{:.2f}".format(amount)
+print(f"Each person should pay: ${amount_per_person}.")
+
+
+# Teacher Solution
+# print("Welcome to the tip calculator!")
+# bill = float(input("How much was the bill? $"))
+# tip = int(input("What percentage of the bill would you like to tip? 10, 12 or 15? "))
+# people = int(input("How many people to split the bill? "))
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+# final_amount = "{:.2f}".format(bill_per_person)
+# print(f"Each person needs to pay: ${final_amount}.")
